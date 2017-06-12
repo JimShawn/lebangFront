c0eb182cf6287317f24a48afb2f9b327a24db7f2
JimShawn <495629394@qq.com>
2017-06-12 17:03:02 +0800

    change server url to inner site

--- app/resource/js/scripts/common/commonProperty.js
+++ app/resource/js/scripts/common/commonProperty.js
@@ -1,7 +1,7 @@
 'use strict';
 define(['app'], function(app){
 	app.constant('commonProperty', {
-		serverHost:'http://121.199.22.27:8080/lebang',
+		serverHost:'http://127.0.0.1:8080/lebang',
 		imgHost:'http://xiangqitestservice.b0.upaiyun.com'
 	})
 })
